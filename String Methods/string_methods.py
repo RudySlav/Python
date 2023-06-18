19e2c67b1bd1138c29910beff13332e12419d355
RudySlav <chrismendel2k@outlook.com>
2023-06-18 21:27:49 +0100

    Did more on editing and changing data types

---
+++ String Methods/string_methods.py
@@ -0,0 +1,12 @@
+name = "krzysztof"
+
+print(len(name)) # Lenght of a string
+print(name.find("t")) # Location of character within string
+print(name.capitalize()) # Capitalizes the first letter
+print(name.upper()) # Makes all letters capital
+print(name.lower()) # Makes all letters lower
+print(name.isdigit()) # True or False if digit
+print(name.isalpha()) # True or False on alpabetic characters
+print(name.count("z")) # Amount of characters in a string
+print(name.replace("z","s")) # Replaces specificied character to new character
+print(name * 3) # Prints string a specific amount
---
+++ Type Casting/type_casting.py
@@ -0,0 +1,14 @@
+# Type Cast = Convert Data Type From One To Another
+
+x = 1 # Int
+y = 2.0 # Float
+z = "3" # String
+
+x = float(x)
+y = float(y)
+z = float(z)
+
+
+print("X is "+str(x))
+print("Y is "+str(y))
+print(z*3)

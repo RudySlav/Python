ec1f3b4bd808265e57577558377e0e1daf83f451
RudySlav <chrismendel2k@outlook.com>
2023-06-19 22:11:36 +0100

    Learned about the math module in python

---
+++ maths.py
@@ -0,0 +1,16 @@
+import math
+
+pi = 3.14
+x = 1
+y = 2
+z = 3
+
+
+print(round(pi))
+print(math.ceil(pi)) # Rounds up to nearest number
+print(math.floor(pi)) # Rounds down to nearest number
+print(abs(pi)) # Distance to 0
+print(pow(pi,2)) # Power of
+print(math.sqrt(pi)) # Square root
+print(max(x,y,z))
+print(min(x,y,z))

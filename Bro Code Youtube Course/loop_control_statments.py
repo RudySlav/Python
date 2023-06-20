f5f80060923f39c13720a71bab87338e80bd3421
RudySlav <chrismendel2k@outlook.com>
2023-06-20 22:33:50 +0100

    Learned about loop control statments

---
+++ Bro Code Youtube Course/loop_control_statments.py
@@ -0,0 +1,22 @@
+# Loop Control Statments = Change a loops execution from its normal sequence
+# break = Used to terminate the loop entirely
+# continue = Skips to the next iteration of the loop
+# pass = does nothing, acts as a placeholder
+
+while True:
+    name = input("Enter your name: ")
+    if name != "":
+        break
+
+phone_number = "056-254-8569"
+
+for i in phone_number:
+    if i == "-":
+        continue
+    print(i, end="")
+
+for i in range(1,21):
+    if i == 13:
+        pass
+    else:
+        print(i)

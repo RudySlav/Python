2e344eff80682aa2013902f53ec5b487126eecab
RudySlav <chrismendel2k@outlook.com>
2023-06-24 20:47:07 +0100

    Coverd 2D lists and navigating them

---
+++ Bro Code Youtube Course/2dlists.py
@@ -0,0 +1,9 @@
+# 2D Lists = A list of lists
+
+drinks = ["coffe", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks,dinner,dessert]
+
+print(food[0][1])

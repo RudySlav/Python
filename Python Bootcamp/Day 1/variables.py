c7527857fac769196fa73a562fe8716eb2175027
RudySlav <chrismendel2k@outlook.com>
2023-06-26 20:44:07 +0100

    Coverd variables

---
+++ Python Bootcamp/Day 1/variables.py
@@ -0,0 +1,17 @@
+#name = "Jack"
+#print(name)
+
+#name = "Kris"
+#print(name)
+
+#name = input("What is your name?")
+#lenght = len(name)
+#print(lenght)
+
+a = input("a: ")
+b = input("b: ")
+
+a,b = b,a
+
+print("a = " + a)
+print("b = " + b)

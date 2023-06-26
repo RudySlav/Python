237794868da0e7c60f8ed18138bcad7959a8533c
RudySlav <chrismendel2k@outlook.com>
2023-06-26 20:53:06 +0100

    Completed the Day 1 Project

---
+++ Python Bootcamp/Challenges/Day_1_project_band_name_generator.py
@@ -0,0 +1,9 @@
+print("***********************************")
+print("* Welcome to band name generator! *")
+print("***********************************")
+
+city = input("What city did you grow up in?: ")
+
+pet = input("What is the name of your pet?: ")
+
+print("Your band name is: \n" + city + " " + pet)

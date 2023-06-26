b58d5d99c6a4283a624156639b1b7ac8fc831172
RudySlav <chrismendel2k@outlook.com>
2023-06-26 19:24:04 +0100

    Started Udemy Course on Python

---
+++ Python Bootcamp/Day 1/printing.py
@@ -0,0 +1,12 @@
+print("Day 1 - Python Print Function")
+print("The function is declared like this!")
+print("print('What to print')")
+
+print("Hello world!\nHello world!") # \n Newline Character
+print("Hello" + " Angela")
+
+# Debuging a problem
+print("Day 1 -String Manipulation")
+print("String Concatenation is done with the '+' sign.")
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")

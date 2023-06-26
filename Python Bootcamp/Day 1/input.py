95d6586a5550df2298355e98f8c604826c04053e
RudySlav <chrismendel2k@outlook.com>
2023-06-26 20:20:27 +0100

    Coverd Input and solved a challenge

---
+++ Python Bootcamp/Day 1/input.py
@@ -0,0 +1,8 @@
+# input() gets user input in console
+# Then print() will print the word "Hello" and the user input
+#print("Hello " + input("What is your name?: ")) # Allows for input from user
+
+
+name = input("What is your name: ")
+
+print("Your name is " + str(len(name)) + " characters long")

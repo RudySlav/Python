d2576a17d7c64a554475822495ba710eb3032d72
Krzysztof Mendel <chrismendel2k@outlook.com>
2023-07-03 19:59:54 +0100

    Coverd if else operator

---
+++ Python Bootcamp/Day 3/if_else_operators.py
@@ -0,0 +1,16 @@
+print("Welcome to the rollercoster!")
+
+height = int(input("What is your height in cm? : "))
+
+if height >= 120:
+    print("You can ride the rollercoster")
+else:
+    print("Sorry, you are too short to ride the rollercoster.")
+
+# > - Greater Than
+# < - Less Than
+# >= - Gratert than or equal to
+# <= - Less than or equal to
+# == - Equal To
+# != - Not Equal to
+

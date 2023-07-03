0061f7095dbe08bf85b4a97d079cfd5d5611682d
Krzysztof Mendel <chrismendel2k@outlook.com>
2023-07-03 10:46:55 +0100

    Coverd basic data types

---
+++ Python Bootcamp/Day 2/data_types.py
@@ -0,0 +1,17 @@
+# Data Types
+
+# String
+print("Hello"[0]) # Pulling Element from string is called Subsript
+print("Hello"[4])
+
+# Integer
+print(123 + 345)
+
+123_456_789 # Underscores act as mathematical commas
+
+# Float
+3.14159
+
+# Boolean
+True
+False

a6757589280669585690ee0b67a2b641c053b729
RudySlav <chrismendel2k@outlook.com>
2023-07-05 19:35:37 +0100

    Coverd Logical Operators

---
+++ Python Bootcamp/Day 3/logical_operators.py
@@ -0,0 +1,10 @@
+# Logical Operators
+
+# True and True = True
+# Both need to be true in and statment for statment to be true
+
+# True or False = True
+# Only one or both need to be true for statment to be true
+
+# not True = False
+# Does opositite of what is asked of it

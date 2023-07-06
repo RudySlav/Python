679ae42803bd4b99c7b705a5aeab0dccd535b657
Krzysztof Mendel <chrismendel2k@outlook.com>
2023-07-06 10:10:49 +0100

    Learned about the random module

---
+++ Python Bootcamp/Day 4/random_module.py
@@ -0,0 +1,17 @@
+import random
+import my_module
+
+random_integer = random.randint(1, 10)
+
+print(random_integer)
+
+print(my_module.pi)
+
+random_float = random.random()
+
+random_float = random_float *5
+
+print(random_float)
+
+love_score = random.randint(1, 100)
+print(f"Your socre is {love_score}")

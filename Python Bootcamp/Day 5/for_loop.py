1e189d90740b3617737ddaaf33fffeec5d4cab46
RudySlav <chrismendel2k@outlook.com>
2024-02-10 22:14:07 +0000

    Did more lessons on for looping, excerices to be done tommorow

--- Python Bootcamp/Day 5/for_loop.py
+++ Python Bootcamp/Day 5/for_loop.py
@@ -1,6 +1,20 @@
 # For Loop
 # Do something to each iteam in a list
-fruits = ["Apple", "Peach", "Pear"]
-for fruit in fruits:
-    print(fruit)
-    print(fruit + " Pie")
+#fruits = ["Apple", "Peach", "Pear"]
+#for fruit in fruits:
+#    print(fruit)
+#    print(fruit + " Pie")
+
+#For Loop Lesson 2
+# Using for loop with range function
+#for number in range (a,b):
+#print number
+
+for number in range(1, 11, 3): #steps by 1 by default, the 3 at the end is the step determinant
+    print(number)
+
+total = 0
+for number in range(1, 101):
+    total += number
+
+print(total)

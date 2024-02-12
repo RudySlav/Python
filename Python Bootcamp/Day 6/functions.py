c11c1f7f910bd9e009f03d6a59545fcc9496d4e9
RudySlav <chrismendel2k@outlook.com>
2024-02-12 21:25:17 +0000

    Was lazy today, coverd some info on functions and did some reborg excercises

---
+++ Python Bootcamp/Day 6/functions.py
@@ -0,0 +1,16 @@
+print() # Determined by ending in parenthesis
+num_char = len("Hello")
+print(num_char)
+
+def first_function():
+    print("Hello")
+    print("Bye")
+
+first_function()
+
+# Steps
+# Define function and name it add () and colon
+# Add in lines of code to perform the function - Dont forget to indent
+# Call function to use it
+
+# Work done on this website - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
